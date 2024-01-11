ec3c8dbba8d5e696a7cd2666912c8ede7efa2c8b
Divin <divinlion80@gmail.com>
2024-01-11 16:10:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import BarChart from './Components/BarChart';
+import { AllData } from './Data';
 
 function App() {
+  const [userData, setUserData] = useState({
+    labels: AllData.map((data) => data.year),
+    datasets: [{
+      label: "Gold Rate",
+      data: AllData.map((data) => data.goldRate),
+      backgroundColor:["gray","yellow"],
+    }]
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{width:700}}>
+      <BarChart chartData={userData} />
+      </div>
     </div>
   );
 }
---
+++ src/Data.js
@@ -0,0 +1,27 @@
+export const AllData = [
+  {
+    id: 1,
+    year: 2019,
+    goldRate: 3522.0,
+  },
+  {
+    id: 2,
+    year: 2020,
+    goldRate: 4865.1,
+  },
+  {
+    id: 3,
+    year: 2021,
+    goldRate: 4872,
+  },
+  {
+    id: 4,
+    year: 2022,
+    goldRate: 5267,
+  },
+  {
+    id: 5,
+    year: 2023,
+    goldRate: 5912,
+  },
+];
